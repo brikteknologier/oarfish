40a03487d831f120371fe28fb9bf1cffabeebf91
Helge Skogly Holm <helge.holm@gmail.com>
2013-10-31 13:39:45 +0100

    Added disposable-redis to tests.

--- test/test_core.js
+++ test/test_core.js
@@ -1,22 +1,32 @@
 var assert = require('assert');
 var redis = require('redis');
 var EventEmitter = require('events').EventEmitter;
+var disposableRedis = require('disposable-redis');
 var coreInit = require('../core');
 
 describe("core", function() {
+  var disposableClient;
   var core;
 
-  before(function() {
-    var db = redis.createClient();
+  before(function(next) {
     var emitter = new EventEmitter();
-    core = coreInit(db, emitter);
+    // First invocation of disposable-redis may need to download and build redis.
+    this.timeout(10 * 60 * 1000);
+    disposableRedis.client(function(err, result) {
+      if (err) return next(err);
+      core = coreInit(result.client, emitter);
+      disposableClient = result;
+      next();
+    });
   });
 
   after(function() {
     core.end();
+    disposableClient.close();
   });
 
   it("exists", function() {
     assert(core, "core not initialized");
   });
+
 });
