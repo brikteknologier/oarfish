ac7f6e7660c8f902c14808be20def593557c21e9
Helge Skogly Holm <helge.holm@gmail.com>
2013-10-31 14:37:26 +0100

    Added more tests, and added placeholders for all missing core tests.

--- test/test_core.js
+++ test/test_core.js
@@ -8,13 +8,24 @@ describe("core", function() {
   var disposableClient;
   var core;
 
+  var notifications = {};
+  function onNotify(jobId, next) {
+    notifications[jobId] = next;
+  }
+  function notifier(emitter, trigger, next) {
+    if (!notifications[trigger.job])
+      return next("no listeners");
+    notifications[trigger.job](null, trigger);
+    delete notifications[trigger.job];
+    next();
+  }
+
   before(function(next) {
-    var emitter = new EventEmitter();
     // First invocation of disposable-redis may need to download and build redis.
     this.timeout(10 * 60 * 1000);
     disposableRedis.client(function(err, result) {
       if (err) return next(err);
-      core = coreInit(result.client, emitter);
+      core = coreInit(result.client, notifier);
       disposableClient = result;
       next();
     });
@@ -35,5 +46,79 @@ describe("core", function() {
 
   it("can receive job status updates", function(next) {
     core.updateStatus("sillyjob", "sillified", next);
+  });
+
+  it("triggers when job status updates to desired state", function(next) {
+    onNotify("importantjob", next);
+
+    var endpoint = "http://example.com/derp";
+    var payload = JSON.stringify("importantjson");
+    core.addTrigger("importantjob", "importantstatus", payload, endpoint, added);
+    
+    function added(err) {
+      if (err) return next(err);
+      core.updateStatus("importantjob", "importantstatus", function(err) {
+        if (err) return next(err);
+      });
+    }
+  });
+  
+  it("sends correct payload on trigger", function(next) {
+    onNotify("correctjob", notificationReceived);
+
+    var endpoint = "http://example.com/derp";
+    var payload = JSON.stringify("the very best of jsons");
+    core.addTrigger("correctjob", "performed", payload, endpoint, added);
+    
+    function added(err) {
+      if (err) return next(err);
+      core.updateStatus("correctjob", "performed", function(err) {
+        if (err) return next(err);
+      });
+    }
+
+    function notificationReceived(err, trigger) {
+      assert.equal(trigger.json, payload);
+      next();
+    }
+  });
+
+  it("does not trigger when job status updates to some other state", function(next) {
+    onNotify("unimportantjob", function() {
+      assert.fail("Should not have received notification.");
+    });
+
+    var endpoint = "http://example.com/derp";
+    var payload = JSON.stringify("importantjson");
+    core.addTrigger("unimportantjob", "goodstatus", payload, endpoint, added);
+    
+    function added(err) {
+      if (err) return next(err);
+      core.updateStatus("importantjob", "badstatus", function(err) {
+        if (err) return next(err);
+      });
+    }
+
+    setTimeout(next, 100);
+  });
+
+  it("retries if notifiee does not respond with 2XX", function(next) {
+    assert.fail("test not implemented");
+  });
+
+  it("triggers when trigger added after status has been updated", function(next) {
+    assert.fail("test not implemented");
+  });
+
+  it("remembers triggers after a restart", function(next) {
+    assert.fail("test not implemented");
+  });
+
+  it("remembers job statuses after a restart", function(next) {
+    assert.fail("test not implemented");
+  });
+
+  it("catches up on retries after a restart", function(next) {
+    assert.fail("test not implemented");
   });
 });
