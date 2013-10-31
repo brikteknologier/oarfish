1b37e8fd821afd2921883003989879cb83b9c3b1
Helge Skogly Holm <helge.holm@gmail.com>
2013-10-31 12:10:43 +0100

    Trigger subscription works.

---
+++ core.js
@@ -0,0 +1,121 @@
+var async = require('async');
+var EventEmitter = require('events').EventEmitter;
+
+var day = 60 * 60 * 24;
+var retainSec = 14 * day;
+var retryNotifyMSec = 3 * 60 * 1000;
+
+retryNotifyMSec = 5000;
+
+/*
+ * "notify" must be an asynchronous function (emitter, trigger, next).  The emitter must
+ * be used to emit 'log' events, and the trigger object must be of the form:
+ * { job: <job id>,
+ *   trigger: <job status causing the trigger>,
+ *   url: <where to send the notification>,
+ *   json: <data to send as the request> }
+ */
+function init(redisClient, notify) {
+  var db = redisClient;
+
+  var emitter = new EventEmitter();
+
+  function addTrigger(jobId, triggerStatus, sendJson, sendUrl, next) {
+    var triggerObject = {
+      job: jobId,
+      trigger: triggerStatus,
+      json: sendJson,
+      url: sendUrl };
+    var triggerJson = JSON.stringify(triggerObject);
+    emitter.emit("log", "Adding trigger: " + triggerJson);
+    db.zadd("triggers", Date.now(), triggerJson, next);
+    db.zremrangebyscore("triggers", "-inf", Date.now() - 1000 * retainSec);
+    tryTrigger(jobId);
+  }
+  
+  function _tryTrigger(jobId, next) {
+    emitter.emit("log", "Checking if job " + jobId + " can be triggered");
+    function triggers(next) {
+      db.zrange("triggers", 0, -1, next);
+    }
+    function jobStatus(next) {
+      db.get("job_" + jobId, next);
+    }
+    function doTrigger(next, res) {
+      function notifyAndDelete(trigger, redisMember) {
+        notify(emitter, trigger, function(err) {
+          if (err) {
+            emitter.emit("log", "Could not send trigger for " + jobId +
+                         " to recipient.  Trying again in " + (retryNotifyMSec / 60000) +
+                         " minutes.");
+            setTimeout(function() { notifyAndDelete(trigger, redisMember); },
+                       retryNotifyMSec);                       
+            return;
+          }
+          emitter.emit("log", "Notification successful. Deleting spent trigger for job " +
+                       jobId);
+          db.zrem("triggers", redisMember);
+        });
+      }
+      var triggers = res.triggers;
+      emitter.emit("log", "Job " + jobId + " has status " + res.jobStatus +
+                   ". Looking for matching triggers...");
+      for (var i = triggers.length - 1; i >= 0; i--) {
+        var trigger = JSON.parse(triggers[i]);
+        emitter.emit("log", " - found trigger for status " + trigger.trigger);
+        if (trigger.job == jobId &&
+            trigger.trigger == res.jobStatus)
+          notifyAndDelete(trigger, triggers[i]);
+      }
+      emitter.emit("log", "...done");
+    }
+    async.auto({
+      triggers: triggers,
+      jobStatus: jobStatus,
+      doTrigger: ["triggers", "jobStatus", doTrigger]
+    }, next);
+  }
+  
+  var triggerQueue = async.queue(_tryTrigger, 1);
+  function tryTrigger(jobId) {
+    triggerQueue.push(jobId, function ignore(){});
+  }
+
+  function updateStatus(jobId, status, next) {
+    emitter.emit("log", "Updating job " + jobId + " status: " + status);
+    db.setex("job_" + jobId, retainSec, status, function(err) {
+      if (err) return next(err);
+      tryTrigger(jobId);
+      next();
+    });
+  }
+
+  function tryTriggerAll() {
+    db.keys("job_*", function(err, keys) {
+      if (err) {
+        emitter.emit("log", "Could not retrieve all job ids: " + err);
+        return;
+      }
+      emitter.emit("log", "Checking all " + keys.length + " jobs for triggering");
+      keys.forEach(function(key) {
+        var jobId = key.slice(4);
+        tryTrigger(jobId);
+      });
+    });
+  }
+
+  function end() {
+    emitter.emit("log", "Closing database connection.");
+    db.end();
+  }
+
+  emitter.updateStatus = updateStatus;
+  emitter.end = end;
+  emitter.addTrigger = addTrigger;
+
+  process.nextTick(tryTriggerAll);
+
+  return emitter;
+}
+
+module.exports = init;
--- oarfish.js
+++ oarfish.js
@@ -1,120 +1,39 @@
-var async = require('async');
 var http = require('http');
+var url = require('url');
+var coreInit = require('./core');
 var redis = require('redis');
-var EventEmitter = require('events').EventEmitter;
 
-var day = 60 * 60 * 24;
-var retainSec = 14 * day;
-var retryNotifyMSec = 3 * 60 * 1000;
-
-function init(logger) {
-  var emitter = new EventEmitter();
-
-  var db = redis.createClient();
-
-  function notify(trigger, next) {
-    emitter.emit("Notifying job " + trigger.job + " status " + trigger.trigger +
-                 " to " + trigger.url);
-    var opts = url.parse(trigger.url);
-    opts.method = "POST";
-    opts.headers = { "Content-Type": "application/json" };
-    var req = http.request(opts, function(res) {
-      if (res.statusCode < 200 || res.statusCode > 299)
-        return next("Did not get 2XX reply from " + trigger.url);
-      next();
-    });
-    req.on('error', next);
-    req.write(trigger.json);
-    req.end();
-  }
-
-  function addTrigger(jobId, triggerStatus, sendJson, sendUrl, next) {
-    var triggerObject = {
-      job: jobId,
-      trigger: triggerStatus,
-      json: sendJson,
-      url: sendUrl };
-    var triggerJson = JSON.stringify(triggerObject);
-    emitter.emit("log", "Adding trigger: " + triggerJson);
-    db.zadd("triggers", Date.now(), triggerJson, next);
-    db.zremrangebyscore("triggers", "-inf", Date.now() - 1000 * retainSec);
-    tryTrigger(jobId);
-  }
-  
-  function _tryTrigger(jobId, next) {
-    emitter.emit("log", "Checking if job " + jobId + " can be triggered");
-    function triggers(next) {
-      db.zrange("triggers", 0, -1, next);
-    }
-    function jobStatus(next) {
-      db.get("job_" + jobId, next);
-    }
-    function doTrigger(next, res) {
-      function notifyAndDelete(trigger, redisMember) {
-        notify(trigger, function(err) {
-          if (err) {
-            emitter.emit("log", "Could not send trigger for " + jobId +
-                         " to recipient.  Trying again in " + (retryNotifyMSec / 60000) +
-                         " minutes.");
-            setTimeout(retryNotifyMSec, notifyTrigger(trigger, redisMember));
-            return;
-          }
-          emitter.emit("log", "Deleting spent trigger for job " + jobId);
-          db.zrem("triggers", redisMember);
-        });
-      }
-      var triggers = res.triggers;
-      for (var i = triggers.length - 1; i >= 0; i--) {
-        var trigger = JSON.parse(triggers[i]);
-        if (trigger.job == jobId &&
-            trigger.status == jobStatus)
-          notifyAndDelete(trigger, triggers[i]);
-      }
-    }
-    async.auto({
-      triggers: triggers,
-      jobStatus: jobStatus,
-      doTrigger: ["triggers", "jobStatus", doTrigger]
-    }, next);
-  }
-
-  var triggerQueue = async.queue(_tryTrigger, 1);
-  function tryTrigger(jobId) {
-    triggerQueue.push(jobId, function ignore(){});
-  }
-
-  function updateStatus(jobId, status, next) {
-    db.setex("job_", jobId, retainSec, status, function(err) {
-      if (err) return next(err);
-      emitter.emit("log", "Job " + jobId + " updated status: " + status);
-      tryTrigger(jobId);
-      next();
-    });
-  }
-
-  function tryTriggerAll() {
-    db.keys("job_*", function(err, keys) {
-      if (err) {
-        emitter.emit("Could not retrieve all job ids: " + err);
-        return;
-      }
-      emitter.emit("Checking all " + keys.length + " jobs for triggering");
-      keys.forEach(function(key) {
-        var jobId = key.slice(4);
-        tryTrigger(jobId);
-      });
-    });
-  }
-
-  function end() {
-    emitter.emit("log", "Closing database connection.");
-    db.end();
-  }
-
-  emitter.updateStatus = updateStatus;
-  emitter.end = end;
-  emitter.tryTriggerAll = tryTriggerAll;
-  emitter.addTrigger = addTrigger;
-
-  return emitter;
+function notify(emitter, trigger, next) {
+  emitter.emit("log", "Notifying job " + trigger.job + " status " + trigger.trigger +
+               " to " + trigger.url);
+  var opts = url.parse(trigger.url);
+  opts.method = "POST";
+  opts.headers = { "Content-Type": "application/json" };
+  var req = http.request(opts, function(res) {
+    if (res.statusCode < 200 || res.statusCode > 299)
+      return next("Did not get 2XX reply from " + trigger.url);
+    next();
+  });
+  req.on('error', next);
+  req.write(trigger.json);
+  req.end();
 }
+
+var core = coreInit(redis.createClient(), notify);
+core.on("log", function(msg) {
+  console.log(msg);
+});
+
+var async = require('async');
+function add(next) {
+  core.addTrigger("job#37", "complacent", JSON.stringify("payload yo"),
+                  "http://localhost:3333", next);
+}
+function complete(next) {
+  core.updateStatus("job#37", "complete", next);
+}
+function complacent(next) {
+  core.updateStatus("job#37", "complacent", next);
+}
+
+async.series([add, complete, complacent], function(err) { if (err) console.log(err); });
