9854014654fab9b1e607875ac1bec79e590c1b59
Helge Skogly Holm <helge.holm@gmail.com>
2013-11-01 14:43:45 +0100

    Reverted brain fart.

--- test/test_core.js
+++ test/test_core.js
@@ -84,10 +84,9 @@ describe("core", function() {
   });
 
   it("does not trigger when job status updates to some other state", function(next) {
-    onNotify("unimportantjob", function(notifyNext) {
+    onNotify("unimportantjob", function() {
       clearTimeout(successTimeout);
       next("Should not have received notification.");
-      notifyNext("I don't want this notification, silly bunny.");
     });
 
     var endpoint = "http://example.com/derp";
