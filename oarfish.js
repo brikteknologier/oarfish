8b0736a7a531d24d9f7130f23fa9ed923f0ee318
Helge Skogly Holm <helge.holm@gmail.com>
2013-11-04 15:06:53 +0100

    Added subscribe endpoint.

--- oarfish.js
+++ oarfish.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var http = require('http');
 var url = require('url');
 var coreInit = require('./core');
@@ -23,3 +24,25 @@ var core = coreInit(redis.createClient(), notify);
 core.on("log", function(msg) {
   console.log(msg);
 });
+
+var app = express();
+app.use(express.bodyParser());
+
+app.post('/subscribe/:jobid/:status', function(req, res, next) {
+  var type = req.get('Content-Type');
+  if (type != 'application/json')
+    return next("Body data must be application/json");
+  console.log(req.body);
+  core.addTrigger(
+    req.params.jobid,
+    req.params.status,
+    req.body.payload,
+    req.body.url,
+    function(err) {
+      if (err) return next(err);
+      res.send("OK");
+    }
+  );
+});
+
+app.listen(9444);
