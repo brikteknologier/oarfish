4fcdd8d97c24bce3c721fc0da23439e3c7d0bb77
Helge Skogly Holm <helge.holm@gmail.com>
2013-11-06 14:25:01 +0100

    Read SNS notification message bodies correctly.

--- amazon-listener.js
+++ amazon-listener.js
@@ -2,26 +2,30 @@ var EventEmitter = require('events').EventEmitter;
 
 function createHandler(updateStatus) {
   var emitter = new EventEmitter();
-
+  
   function handler(req, res, next) {
-    var body = req.body;
-    if (body == undefined)
-      throw new Error("req.body not set.  express.bodyParser() not in use by app?");
-
-    // Amazon SNS sends JSON body as ContentType=text/plain which
-    // doesn't trigger body parsing.
-    if (typeof body != 'object')
-      body = JSON.parse(body);
-
-    emitter.emit('log', "Notification received: " + body.Subject || body);
-    var message = JSON.parse(body.Message);
-
-    updateStatus(message.jobId, message.state, function(err) {
-      if (err) return next(err);
-      res.send(["KTHX","ASUM","OK","SWEET","K","LOL","GOOD","NOTED"][Date.now()%8]);
+    var body = '';
+    var notification;
+    
+    req.on('data', function(data) { body += data; });
+    req.on('end', function() {
+      notification = JSON.parse(body);
+      emitter.emit('log', "Notification received: " + notification.Subject || notification);
+      
+      try {
+        var message = JSON.parse(notification.Message);
+      } catch (error) {
+        emitter.emit('log', "Notification Message not JSON: " + notification.Message);
+        return next(error);
+      }
+      
+      updateStatus(message.jobId, message.state, function(err) {
+        if (err) return next(err);
+        res.send(["KTHX","ASUM","OK","SWEET","K","LOL","GOOD","NOTED"][Date.now()%8]);
+      });
     });
   }
-
+  
   emitter.handlePost = handler;
 
   return emitter;
